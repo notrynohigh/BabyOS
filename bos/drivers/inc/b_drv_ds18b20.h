39f5509da8cbd2db30b33c418ca0efa40b645256
polyGithub <baoli.chen@outlook.com>
2021-04-30 10:47:04 +0800

    单总线通讯，DS18B20温度传感器驱动

---
+++ bos/drivers/inc/b_drv_ds18b20.h
@@ -0,0 +1,89 @@
+/**
+ *!
+ * \file        b_drv_ds18b20.h
+ * \version     v0.0.1
+ * \date        2021/04/29
+ * \author      poly(notrynohigh@outlook.com)
+ *******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2020 poly
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************
+ */
+#ifndef __B_DRV_DS18B20H__
+#define __B_DRV_DS18B20H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*Includes ----------------------------------------------*/
+#include "b_drv_class_gsensor.h"
+/**
+ * \addtogroup B_DRIVER
+ * \{
+ */
+
+/**
+ * \addtogroup DS18B20
+ * \{
+ */
+
+/**
+ * \defgroup DS18B20_Exported_TypesDefinitions
+ * \{
+ */
+
+typedef bDriverInterface_t bDS18B20_Driver_t;
+//static bUtilSbus_t SBUS_IO = {
+//    .sBusIo = {B_HAL_GPIOA, B_HAL_PIN8},
+//};
+
+/**
+ * \}
+ */
+
+/**
+ * \defgroup DS18B20_Exported_Defines
+ * \{
+ */
+/** Device Identification (Who am I) **/
+
+
+/**
+ * \}
+ */
+
+/**
+ * \}
+ */
+
+/**
+ * \}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/************************ Copyright (c) 2020 Bean *****END OF FILE****/
