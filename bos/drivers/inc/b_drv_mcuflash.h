b814f9a6df71f09f085edab71da00b49387bc8cb
notrynohigh <notrynohigh@outlook.com>
2021-06-13 22:37:59 +0800

    1.将mcu片内flash作为设备，让其可以使用kv

---
+++ bos/drivers/inc/b_drv_mcuflash.h
@@ -0,0 +1,73 @@
+/**
+ *!
+ * \file        b_drv_mcuflash.h
+ * \version     v0.0.1
+ * \date        2021/06/13
+ * \author      Bean(notrynohigh@outlook.com)
+ *******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2021 Bean
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************
+ */
+#ifndef __B_DRV_MCUFLASH_H__
+#define __B_DRV_MCUFLASH_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*Includes ----------------------------------------------*/
+#include "drivers/inc/b_driver.h"
+/**
+ * \addtogroup B_DRIVER
+ * \{
+ */
+
+/**
+ * \addtogroup MCUFLASH
+ * \{
+ */
+
+/**
+ * \defgroup MCUFLASH_Exported_TypesDefinitions
+ * \{
+ */
+typedef bDriverInterface_t bMCUFLASH_Driver_t;
+/**
+ * \}
+ */
+
+/**
+ * \}
+ */
+
+/**
+ * \}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/************************ Copyright (c) 2021 Bean *****END OF FILE****/
