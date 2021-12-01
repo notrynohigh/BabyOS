9ce6a72d9ace2848179094fba61c2978ade90f26
haimeng.chen <85038427@qq.com>
2021-12-01 16:46:35 +0800

    添加STM32G0系列BOS外设，未测试；

---
+++ bos/mcu/st/stm32g0x0/mcu_stm32g0x0_flash.c
@@ -0,0 +1,186 @@
+/**
+ *!
+ * \file        mcu_stm32f10x_flash.c
+ * \version     v0.0.1
+ * \date        2021/06/13
+ * \author      Bean(notrynohigh@outlook.com)
+ *******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2021 Bean
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SGPIOL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************
+ */
+
+/*Includes ----------------------------------------------*/
+#include <string.h>
+
+#include "b_config.h"
+#include "hal/inc/b_hal_flash.h"
+
+#if (_MCU_PLATFORM == 1101 || _MCU_PLATFORM == 1102 || _MCU_PLATFORM == 1103)
+
+#define FLASH_BASE_ADDR (0x8000000UL)
+
+#define FLASH_PAGE_SIZE (2048)
+
+#if (_MCU_PLATFORM == 1101)
+#define FLASH_MAX_SIZE (64 * 1024)
+#elif (_MCU_PLATFORM == 1102)
+#define FLASH_MAX_SIZE (128 * 1024)
+#elif (_MCU_PLATFORM == 1103)
+#define FLASH_MAX_SIZE (512 * 1024)
+#endif
+
+#define FLASH_KEY_1 (0x45670123UL)
+#define FLASH_KEY_2 (0xCDEF89ABUL)
+#define FLASH_PER_TIMEOUT (0x000B0000UL)
+#define FLASH_PG_TIMEOUT (0x00002000UL)
+
+typedef struct
+{
+    volatile uint32_t ACR;
+    volatile uint32_t KEYR;
+    volatile uint32_t OPTKEYR;
+    volatile uint32_t SR;
+    volatile uint32_t CR;
+    volatile uint32_t AR;
+    volatile uint32_t RESERVED;
+    volatile uint32_t OBR;
+    volatile uint32_t WRPR;
+} McuFlashReg_t;
+
+#define MCU_FLASH ((McuFlashReg_t *)0x40022000)
+
+static int _FlashInit()
+{
+    return 0;
+}
+
+static int _FlashUnlock()
+{
+    int retval      = 0;
+    MCU_FLASH->KEYR = FLASH_KEY_1;
+    MCU_FLASH->KEYR = FLASH_KEY_2;
+    return retval;
+}
+
+static int _FlashLock()
+{
+    int retval = 0;
+    MCU_FLASH->CR |= (0x00000001 << 7);
+    return retval;
+}
+
+static int _FlashErase(uint32_t raddr, uint8_t pages)
+{
+    int     retval  = 0;
+    int     timeout = 0;
+    uint8_t i       = 0;
+
+    raddr = FLASH_BASE_ADDR + raddr;
+    raddr = raddr / FLASH_PAGE_SIZE * FLASH_PAGE_SIZE;
+    if ((raddr + (pages * FLASH_PAGE_SIZE)) > (FLASH_BASE_ADDR + FLASH_MAX_SIZE) ||
+        ((MCU_FLASH->SR) & 0x01) != 0)
+    {
+        return -1;
+    }
+
+    for (i = 0; i < pages; i++)
+    {
+        MCU_FLASH->SR |= 0xFC;
+        MCU_FLASH->CR |= (0x00000001 << 1);
+        MCU_FLASH->AR = raddr;
+        MCU_FLASH->CR |= (0x00000001 << 6);
+        timeout = FLASH_PER_TIMEOUT;
+        while (((MCU_FLASH->SR) & 0x01) != 0 && timeout > 0)
+        {
+            timeout--;
+        }
+        MCU_FLASH->CR &= ~(0x00000001 << 1);
+        if (timeout <= 0)
+        {
+            retval = -2;
+            break;
+        }
+        raddr += FLASH_PAGE_SIZE;
+    }
+    return retval;
+}
+
+static int _FlashWrite(uint32_t raddr, const uint8_t *pbuf, uint16_t len)
+{
+    int      timeout = 0;
+    uint16_t wdata   = 0;
+    uint16_t wlen = (len + 1) / 2, i = 0;
+    raddr = FLASH_BASE_ADDR + raddr;
+    if (pbuf == NULL || (raddr & 0x1) || (raddr + len) > (FLASH_MAX_SIZE + FLASH_BASE_ADDR) ||
+        ((MCU_FLASH->SR) & 0x01) != 0)
+    {
+        return -1;
+    }
+
+    for (i = 0; i < wlen; i++)
+    {
+        wdata = (wdata << 8) | pbuf[i * 2 + 1];
+        wdata = (wdata << 8) | pbuf[i * 2 + 0];
+
+        MCU_FLASH->SR |= 0xFC;
+        MCU_FLASH->CR |= (0x00000001 << 0);
+        *((volatile uint16_t *)raddr) = wdata;
+
+        timeout = FLASH_PG_TIMEOUT;
+        while (((MCU_FLASH->SR) & 0x01) != 0 && timeout > 0)
+        {
+            timeout--;
+        }
+        MCU_FLASH->CR &= ~(0x00000001 << 0);
+        if (timeout <= 0)
+        {
+            return -2;
+        }
+        raddr += 2;
+    }
+    return (wlen * 2);
+}
+
+static int _FlashRead(uint32_t raddr, uint8_t *pbuf, uint16_t len)
+{
+    if (pbuf == NULL || (raddr + FLASH_BASE_ADDR + len) > (FLASH_MAX_SIZE + FLASH_BASE_ADDR))
+    {
+        return -1;
+    }
+    raddr = FLASH_BASE_ADDR + raddr;
+    memcpy(pbuf, (const uint8_t *)raddr, len);
+    return len;
+}
+
+bHalFlashDriver_t bHalFlashDriver = {
+    .pFlashInit   = _FlashInit,
+    .pFlashUnlock = _FlashUnlock,
+    .pFlashLock   = _FlashLock,
+    .pFlashErase  = _FlashErase,
+    .pFlashWrite  = _FlashWrite,
+    .pFlashRead   = _FlashRead,
+};
+
+#endif
+
+/************************ Copyright (c) 2021 Bean *****END OF FILE****/
---
+++ bos/mcu/st/stm32g0x0/mcu_stm32g0x0_spi.c
@@ -0,0 +1,179 @@
+/**
+ *!
+ * \file        mcu_stm32f10x_spi.c
+ * \version     v0.0.1
+ * \date        2020/03/25
+ * \author      Bean(notrynohigh@outlook.com)
+ *******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2020 Bean
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SSPIL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************
+ */
+
+/*Includes ----------------------------------------------*/
+#include "b_config.h"
+#include "hal/inc/b_hal_spi.h"
+#include "utils/inc/b_util_spi.h"
+
+#if (_MCU_PLATFORM == 1101 || _MCU_PLATFORM == 1102 || _MCU_PLATFORM == 1103)
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+//         Register Address
+
+#define SPI1_BASE_ADDR (0x40013000)
+#define SPI2_BASE_ADDR (0x40003800)
+#define SPI3_BASE_ADDR (0x40003C00)
+
+typedef struct
+{
+    volatile uint16_t CR1;
+    uint16_t          RESERVED0;
+    volatile uint16_t CR2;
+    uint16_t          RESERVED1;
+    volatile uint16_t SR;
+    uint16_t          RESERVED2;
+    volatile uint16_t DR;
+    uint16_t          RESERVED3;
+    volatile uint16_t CRCPR;
+    uint16_t          RESERVED4;
+    volatile uint16_t RXCRCR;
+    uint16_t          RESERVED5;
+    volatile uint16_t TXCRCR;
+    uint16_t          RESERVED6;
+    volatile uint16_t I2SCFGR;
+    uint16_t          RESERVED7;
+    volatile uint16_t I2SPR;
+    uint16_t          RESERVED8;
+} McuSpiReg_t;
+
+#define MCU_SPI1 ((McuSpiReg_t *)SPI1_BASE_ADDR)
+#define MCU_SPI2 ((McuSpiReg_t *)SPI2_BASE_ADDR)
+#define MCU_SPI3 ((McuSpiReg_t *)SPI3_BASE_ADDR)
+
+static McuSpiReg_t *SpiTable[3] = {MCU_SPI1, MCU_SPI2, MCU_SPI3};
+
+static int _SpiSetSpeed(bHalSPIIf_t *spi_if, bHalSPISpeed_t speed)
+{
+    uint16_t     SpeedVal = 1;
+    McuSpiReg_t *pSpi     = NULL;
+    if (IS_NULL(spi_if) || (speed >= B_HAL_SPI_SPEED_INVALID))
+    {
+        return -1;
+    }
+    if (speed == B_HAL_SPI_SLOW)
+    {
+        SpeedVal = 6;
+    }
+    if (spi_if->_if.spi > B_HAL_SPI_3)
+    {
+        return -1;
+    }
+    pSpi = SpiTable[spi_if->_if.spi];
+    while (pSpi->SR & 0x80)
+    {
+        ;
+    }
+    pSpi->CR1 &= ~(0x7 << 3);
+    pSpi->CR1 |= (SpeedVal << 3);
+    return 0;
+}
+
+static uint8_t _SpiTransfer(bHalSPIIf_t *spi_if, uint8_t dat)
+{
+    uint8_t      tmp;
+    bUtilSPI_t   simulating_spi;
+    McuSpiReg_t *pSpi = NULL;
+    if (IS_NULL(spi_if))
+    {
+        return 0;
+    }
+    if (spi_if->is_simulation == 1)
+    {
+        simulating_spi.clk  = spi_if->_if.simulating_spi.clk;
+        simulating_spi.mosi = spi_if->_if.simulating_spi.mosi;
+        simulating_spi.miso = spi_if->_if.simulating_spi.miso;
+        simulating_spi.CPHA = spi_if->_if.simulating_spi.CPHA;
+        simulating_spi.CPOL = spi_if->_if.simulating_spi.CPOL;
+        tmp                 = bUtilSPI_WriteRead(simulating_spi, dat);
+    }
+    else
+    {
+        if (spi_if->_if.spi > B_HAL_SPI_3)
+        {
+            return 0;
+        }
+        pSpi = SpiTable[spi_if->_if.spi];
+        while ((pSpi->SR & 0x02) == 0)
+        {
+            ;
+        }
+        pSpi->DR = dat;
+        while ((pSpi->SR & 0x01) == 0)
+        {
+            ;
+        }
+        tmp = pSpi->DR;
+    }
+    return tmp;
+}
+
+static int _SpiSend(bHalSPIIf_t *spi_if, const uint8_t *pbuf, uint16_t len)
+{
+    int i = 0;
+    if (IS_NULL(spi_if) || IS_NULL(pbuf))
+    {
+        return -1;
+    }
+    for (i = 0; i < len; i++)
+    {
+        _SpiTransfer(spi_if, pbuf[i]);
+    }
+    return 0;
+}
+
+static int _SpiReceive(bHalSPIIf_t *spi_if, uint8_t *pbuf, uint16_t len)
+{
+    int i = 0;
+    if (IS_NULL(spi_if) || IS_NULL(pbuf))
+    {
+        return -1;
+    }
+    for (i = 0; i < len; i++)
+    {
+        pbuf[i] = _SpiTransfer(spi_if, 0xff);
+    }
+    return 0;
+}
+
+bHalSPIDriver_t bHalSPIDriver = {
+    .pSetSpeed = _SpiSetSpeed,
+    .pSend     = _SpiSend,
+    .pReceive  = _SpiReceive,
+    .pTransfer = _SpiTransfer,
+};
+
+#endif
+
+/************************ Copyright (c) 2020 Bean *****END OF FILE****/
