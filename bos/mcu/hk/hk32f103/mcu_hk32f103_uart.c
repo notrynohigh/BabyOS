76fbd32ecfe86d5eff75ca1c680383577c21fba1
notrynohigh <notrynohigh>
2024-12-15 01:31:48 +0800

    feat:#I8EZIK 修改spi和uart适配代码

--- bos/mcu/hk/hk32f103/mcu_hk32f103_uart.c
+++ bos/mcu/hk/hk32f103/mcu_hk32f103_uart.c
@@ -1,69 +1,32 @@
 #include "b_config.h"
 #include "hal/inc/b_hal_uart.h"
+#if !defined(HK32F103C8XX) && !defined(HK32F103CBXX) && !defined(HK32F103R8XX) && \
+    !defined(HK32F103RBXX) && !defined(HK32F103V8XX) && !defined(HK32F103VBXX) && \
+    !defined(HK32F103RCXX) && !defined(HK32F103RDXX) && !defined(HK32F103REXX) && \
+    !defined(HK32F103VCXX) && !defined(HK32F103VDXX) && !defined(HK32F103VEXX)
 
-#define UART1_BASE_ADDR (0x40013800)
-#define UART2_BASE_ADDR (0x40004400)
-#define UART3_BASE_ADDR (0x40004800)
-#define UART4_BASE_ADDR (0x40004C00)
-#define UART5_BASE_ADDR (0x40005000)
+#error \
+    "Please select first the target HK32F10x device used in your application (in HK32f10x.h file)"
 
-typedef struct
-{
-    volatile uint16_t SR;
-    uint16_t          RESERVED0;
-    volatile uint16_t DR;
-    uint16_t          RESERVED1;
-    volatile uint16_t BRR;
-    uint16_t          RESERVED2;
-    volatile uint16_t CR1;
-    uint16_t          RESERVED3;
-    volatile uint16_t CR2;
-    uint16_t          RESERVED4;
-    volatile uint16_t CR3;
-    uint16_t          RESERVED5;
-    volatile uint16_t GTPR;
-    uint16_t          RESERVED6;
-} McuUartReg_t;
+#else
 
-#define MCU_UART1 ((McuUartReg_t *)UART1_BASE_ADDR)
-#define MCU_UART2 ((McuUartReg_t *)UART2_BASE_ADDR)
-#define MCU_UART3 ((McuUartReg_t *)UART3_BASE_ADDR)
-#define MCU_UART4 ((McuUartReg_t *)UART4_BASE_ADDR)
-#define MCU_UART5 ((McuUartReg_t *)UART5_BASE_ADDR)
+#include "hk32f10x.h"
 
-static McuUartReg_t *UartTable[5] = {MCU_UART1, MCU_UART2, MCU_UART3, MCU_UART4, MCU_UART5};
+static USART_TypeDef *bMcuUartTable[] = {USART1, USART2, USART3, UART4, UART5};
 
 int bMcuUartSend(bHalUartNumber_t uart, const uint8_t *pbuf, uint16_t len)
 {
-    int           i       = 0;
-    int           timeout = 0x000B0000;
-    McuUartReg_t *pUart   = NULL;
-    if (uart > B_HAL_UART_5 || pbuf == NULL)
+    if (uart <= B_HAL_UART_5)
     {
-        return -1;
-    }
-    pUart = UartTable[uart];
-    for (i = 0; i < len; i++)
-    {
-        timeout = 0x000B0000;
-        while (timeout > 0 && ((pUart->SR & (0x1 << 6)) == 0))
+        for (int i = 0; i < len; i++)
         {
-            timeout--;
+            while (USART_GetFlagStatus(bMcuUartTable[uart], USART_FLAG_TXE) != SET)
+                ;
+            USART_SendData(bMcuUartTable[uart], pbuf[i]);
         }
-        if (timeout <= 0)
-        {
-            return -2;
-        }
-        pUart->DR = pbuf[i];
+        return 0;
     }
-    timeout = 0x000B0000;
-    while (timeout > 0 && ((pUart->SR & (0x1 << 6)) == 0))
-    {
-        timeout--;
-    }
-    if (timeout <= 0)
-    {
-        return -2;
-    }
-    return len;
+    return -1;
 }
+
+#endif
