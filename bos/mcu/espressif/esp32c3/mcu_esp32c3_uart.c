19865d48d972b091c9f00f6c4e0f0d72eb445a27
notrynohigh <notrynohigh>
2025-02-09 21:17:42 +0800

    feat:#IBJUIV add files

---
+++ bos/mcu/espressif/esp32c3/mcu_esp32c3_uart.c
@@ -0,0 +1,13 @@
+#include "b_config.h"
+#include "hal/inc/b_hal_uart.h"
+
+int bMcuUartSend(bHalUartNumber_t uart, const uint8_t *pbuf, uint16_t len)
+{
+    return -1;
+}
+
+int bMcuUartReceiveDma(bHalUartNumber_t uart, bHalDmaConfig_t *pconf)
+{
+    return -1;
+}
+
